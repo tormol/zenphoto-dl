bc416c252b5b05d55534a2dbb08d57a167201164
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2018-01-03 20:00:16 +0100

    Fix whitespace
    * Remove trailing whitespace
    * Use tabs for indentation everywhere
    * Use two blank lines between functions

--- dl.py
+++ dl.py
@@ -2,38 +2,40 @@
 import os, io, time
 import urllib.request, urllib.parse
 import lxml, lxml.html, lxml.etree
+
 html_parser = lxml.etree.HTMLParser(remove_blank_text=True, remove_comments=False, no_network=True)
+
 
 # extract file name if none from url, and download if it doesn't exist
 # returns the download path and whether the file was (successfully) downloaded
 # prints error on 404s, and throws on all other errors
 def download(url, keep_params=True, to=None, default_dir="website_html", keep_domain=False):
-    if to is None:
-        _prot, _, name = url.partition("//") # strip http[s]://
-        #name = url.rsplit("/", 1)[1] # strip path
-        if not keep_domain:
-            _domain, _, name = name.partition("/")
-        name = name.rstrip("/") # remove trailing slash
-        name = name.replace("/", "--") # make it a valid linux filename that doesn't confuse firefox (it treats \ as /)
-        to = os.path.join(default_dir, name)
-    if not keep_params:
-        to, _, _params = to.partition("?")
-    else:
-        to = to.replace("?", "-P").replace("&", "-P") # for php -S
-    exists = os.path.isfile(to)
-    if not exists:
-        print("<  "+url)
-        print(">  "+to)
-        time.sleep(2) # good bot
-        try:
-            urllib.request.urlretrieve(url, to)
-        except urllib.error.HTTPError as err:
-            if err.code == 404:
-                print("\tNOT FOUND")
-                return None, False
-            else:
-                raise err
-    return to, not exists
+	if to is None:
+		_prot, _, name = url.partition("//") # strip http[s]://
+		#name = url.rsplit("/", 1)[1] # strip path
+		if not keep_domain:
+			_domain, _, name = name.partition("/")
+		name = name.rstrip("/") # remove trailing slash
+		name = name.replace("/", "--") # make it a valid linux filename that doesn't confuse firefox (it treats \ as /)
+		to = os.path.join(default_dir, name)
+	if not keep_params:
+		to, _, _params = to.partition("?")
+	else:
+		to = to.replace("?", "-P").replace("&", "-P") # for php -S
+	exists = os.path.isfile(to)
+	if not exists:
+		print("<  "+url)
+		print(">  "+to)
+		time.sleep(2) # good bot
+		try:
+			urllib.request.urlretrieve(url, to)
+		except urllib.error.HTTPError as err:
+			if err.code == 404:
+				print("\tNOT FOUND")
+				return None, False
+			else:
+				raise err
+	return to, not exists
 
 base_url = "http://archives.pawpet.tv"
 
@@ -41,6 +43,7 @@ def get_html(url):
 	path, _ = download(url, keep_params=True)
 	html = lxml.html.parse(path, parser=html_parser, base_url=base_url)
 	return html.getroot()
+
 
 def get_img_url(page_url):
 	p = {}
@@ -54,6 +57,7 @@ def get_img_url(page_url):
 	name, _, ext = urllib.parse.unquote(p["image"]).rpartition(".")
 	return img_url, name, ext
 
+
 def parse_index(html):
 	"""returns [{url:,title:,thumb:}], [(url,name)]"""
 	albums = {}
@@ -66,17 +70,17 @@ def parse_index(html):
 	albums = sorted(albums.values(), key=lambda album: album["title"], reverse=True)
 	for album in albums:
 		album["title"] = album["title"].replace("_"," ").strip()
-	
+
 	images = []
 	for imglink in html.cssselect("#images .image a"):
 		imgpage = base_url+imglink.get("href")
 		imgname = imglink.get("title")
 		images.append((imgpage,imgname))
-		
+
 	next = html.cssselect("ul.pagelist li.next a")
 	next = None if len(next) == 0 else base_url + next[0].get("href")
-
 	return albums, images, next
+
 
 def crawl_album(url, path, indent=""):
 	html = get_html(url)
@@ -119,6 +123,7 @@ def crawl_album(url, path, indent=""):
 	if next_page is not None:
 		print("%snext page for %s ..." % (indent, path))
 		crawl_album(next_page, path, indent)
+
 
 try:
 	os.makedirs("website_html", mode=0o755)
